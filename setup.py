dcac3e0d55dca1dc4b676861393d5d1463062213
Ashish Barman <ashbarman@gdt-labs.local>
2019-09-22 21:54:38 -0500

    updates

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='keras_lr_finder',
       version='0.1',
       description='Learning rate finder for Keras.',
-      url='http://github.com/surmenok/keras_lr_finder',
-      author='Pavel Surmenok',
-      author_email='pavel@surmenok.com',
+      url='https://github.com/sbarman-mi9/keras_lr_finder',
+      author='Snehasish Barman',
+      author_email='sbarman@wpi.edu',
       license='MIT',
       packages=['keras_lr_finder'],
       install_requires=[
